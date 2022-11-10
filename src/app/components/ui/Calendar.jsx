3518cc2c48847acad0aef8312ed9f6ece8bfeeec
George Roubie <george.roubie@gmail.com>
2022-11-10 23:36:47 +0200

    Update Calendar.jsx

--- src/app/components/ui/Calendar.jsx
+++ src/app/components/ui/Calendar.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import _CircleSnowFlake from '../icons/CircleSnowFlake';
+import _SimpleSnowFlake from '../icons/SimpleSnowFlake';
+import _StarSnowFlake from '../icons/StarSnowFlake';
 
 const Wrapper = styled.div`
   border: ${({ theme: { borderSize, colors } }) => `${borderSize.large} solid ${colors.borderPrimary}`};
   width: 100%;
   height: auto;
-  border-radius: 10px;
-  overflow: hidden;
+  border-radius: ${({ theme: { borderRadius } }) => borderRadius.normal};
+  position: relative;
 `;
 
 const InnerWrapper = styled.div`
+  overflow: hidden;
+`;
+
+const DaysList = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: -${({ theme: { borderSize } }) => borderSize.small};
@@ -27,6 +34,85 @@ const Day = styled.div`
   font-weight: ${({ theme: { fontWeight } }) => fontWeight.bold};
   border-bottom: ${({ theme: { borderSize, colors } }) => `${borderSize.small} solid ${colors.borderPrimary}`};
   border-right: ${({ theme: { borderSize, colors } }) => `${borderSize.small} solid ${colors.borderPrimary}`};
+`;
+
+const SnowFlake = css`
+  width: ${({ theme: { fontSize } }) => fontSize.large};
+  height: ${({ theme: { fontSize } }) => fontSize.large};
+  fill: ${({ theme: { colors } }) => colors.textPrimary};
+  position: absolute;
+`;
+
+const TopLeftCircleSnowFlake = styled(_CircleSnowFlake)`
+  ${SnowFlake}
+  top: -1.2rem;
+  left: -1.2rem;
+`;
+
+const TopLeftSimpleSnowFlake = styled(_SimpleSnowFlake)`
+  ${SnowFlake}
+  top: -1.5rem;
+  left: 0;
+`;
+
+const TopLeftStarSnowFlake = styled(_StarSnowFlake)`
+  ${SnowFlake}
+  top: 0;
+  left: -1.5rem;
+`;
+
+const TopRightCircleSnowFlake = styled(_CircleSnowFlake)`
+  ${SnowFlake}
+  top: -1.2rem;
+  right: -1.2rem;
+`;
+
+const TopRightSimpleSnowFlake = styled(_SimpleSnowFlake)`
+  ${SnowFlake}
+  top: -1.5rem;
+  right: 0;
+`;
+
+const TopRightStarSnowFlake = styled(_StarSnowFlake)`
+  ${SnowFlake}
+  top: 0;
+  right: -1.5rem;
+`;
+
+const BottomLeftCircleSnowFlake = styled(_CircleSnowFlake)`
+  ${SnowFlake}
+  bottom: -1.2rem;
+  left: -1.2rem;
+`;
+
+const BottomLeftSimpleSnowFlake = styled(_SimpleSnowFlake)`
+  ${SnowFlake}
+  bottom: -1.5rem;
+  left: 0;
+`;
+
+const BottomLeftStarSnowFlake = styled(_StarSnowFlake)`
+  ${SnowFlake}
+  bottom: 0;
+  left: -1.5rem;
+`;
+
+const BottomRightCircleSnowFlake = styled(_CircleSnowFlake)`
+  ${SnowFlake}
+  bottom: -1.2rem;
+  right: -1.2rem;
+`;
+
+const BottomRightSimpleSnowFlake = styled(_SimpleSnowFlake)`
+  ${SnowFlake}
+  bottom: -1.5rem;
+  right: 0;
+`;
+
+const BottomRightStarSnowFlake = styled(_StarSnowFlake)`
+  ${SnowFlake}
+  bottom: 0;
+  right: -1.5rem;
 `;
 
 const Calendar = () => {
@@ -52,12 +138,26 @@ const Calendar = () => {
 
   return (
     <Wrapper>
+      <TopLeftStarSnowFlake />
+      <TopLeftSimpleSnowFlake />
+      <TopLeftCircleSnowFlake />
+      <TopRightCircleSnowFlake />
+      <TopRightSimpleSnowFlake />
+      <TopRightStarSnowFlake />
+      <BottomLeftCircleSnowFlake />
+      <BottomLeftSimpleSnowFlake />
+      <BottomLeftStarSnowFlake />
+      <BottomRightCircleSnowFlake />
+      <BottomRightSimpleSnowFlake />
+      <BottomRightStarSnowFlake />
       <InnerWrapper>
-        {days.map((d) => (
-          <Day key={d} ref={dayRef} style={{ height: width }}>
-            {d}
-          </Day>
-        ))}
+        <DaysList>
+          {days.map((d) => (
+            <Day key={d} ref={dayRef} style={{ height: width }}>
+              {d}
+            </Day>
+          ))}
+        </DaysList>
       </InnerWrapper>
     </Wrapper>
   );
