52969697460959c9bf464ba99059f27269af2697
George Roubie <george.roubie@gmail.com>
2022-11-14 23:42:06 +0200

    Update Loader.jsx

--- src/app/routing/Loader.jsx
+++ src/app/routing/Loader.jsx
@@ -1,9 +1,23 @@
 import PropTypes from 'prop-types';
 import { Suspense } from 'react';
+import Container from '../components/layout/Container';
+import PageWrapper from '../components/layout/PageWrapper';
 import Description from '../components/typography/Description';
 
 const RoutingLoader = ({ children }) => {
-  return <Suspense fallback={<Description>Loading...</Description>}>{children}</Suspense>;
+  return (
+    <Suspense
+      fallback={
+        <PageWrapper>
+          <Container>
+            <Description>Loading...</Description>
+          </Container>
+        </PageWrapper>
+      }
+    >
+      {children}
+    </Suspense>
+  );
 };
 
 RoutingLoader.propTypes = {
