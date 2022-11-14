a602051689964bedc2aa725281574af12ac8924b
George Roubie <george.roubie@gmail.com>
2022-11-14 22:57:59 +0200

    Update PageWrapper.jsx

--- src/app/components/layout/PageWrapper.jsx
+++ src/app/components/layout/PageWrapper.jsx
@@ -1,14 +1,34 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import SnowFlakes from './SnowFlakes';
 
-const Wrapper = styled.main`
+const Wrapper = styled.div`
   margin: ${({ theme: { spacing } }) => spacing.xlarge} auto;
   padding: 0 ${({ theme: { spacing } }) => spacing.large};
   max-width: 800px;
 `;
 
+const InnerWrapper = styled.div`
+  border: ${({ theme: { borderSize, colors } }) => `${borderSize.large} solid ${colors.borderPrimary}`};
+  width: 100%;
+  height: auto;
+  border-radius: ${({ theme: { borderRadius } }) => borderRadius.normal};
+  position: relative;
+`;
+
+const ChildrenWrapper = styled.div`
+  overflow: hidden;
+`;
+
 const PageWrapper = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <SnowFlakes />
+        <ChildrenWrapper>{children}</ChildrenWrapper>
+      </InnerWrapper>
+    </Wrapper>
+  );
 };
 
 PageWrapper.propTypes = {
