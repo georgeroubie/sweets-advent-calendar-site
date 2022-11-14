b5bbd82b9a577476f525404d406cf842d5b731d2
George Roubie <george.roubie@gmail.com>
2022-11-14 23:43:09 +0200

    Update DecemberRecipe22.jsx

--- src/app/components/recipes/DecemberRecipe22.jsx
+++ src/app/components/recipes/DecemberRecipe22.jsx
@@ -1,5 +1,21 @@
-const DecemberRecipe22 = () => {
-  return <div>DecemberRecipe22</div>;
+import PropTypes from 'prop-types';
+import Container from '../layout/Container';
+import Title from '../typography/Title';
+
+const DecemberRecipe22 = ({ setActiveRecipe }) => {
+  function resetActiveRecipe() {
+    setActiveRecipe(undefined);
+  }
+
+  return (
+    <Container>
+      <Title onBack={resetActiveRecipe}>December Recipe 22</Title>
+    </Container>
+  );
+};
+
+DecemberRecipe22.propTypes = {
+  setActiveRecipe: PropTypes.func.isRequired,
 };
 
 export default DecemberRecipe22;
