0925d15beb5cde78052354c02515b6c8d343e05f
George Roubie <george.roubie@gmail.com>
2022-11-14 23:41:57 +0200

    Update base.js

--- src/app/theme/themes/base.js
+++ src/app/theme/themes/base.js
@@ -27,16 +27,9 @@ const baseTheme = {
     xlarge: '3rem',
   },
   fontWeight: {
-    thin: 100,
-    extraLight: 200,
-    light: 300,
     normal: 400,
-    medium: 500,
     semiBold: 600,
     bold: 700,
-    extraBold: 800,
-    black: 900,
-    extraBlack: 950,
   },
 };
 
