a8c886aae84e45040b13cce56ef8602c350873e7
George Roubie <george.roubie@gmail.com>
2022-11-14 23:41:59 +0200

    Update typography.js

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -2,13 +2,17 @@ import { css } from 'styled-components';
 
 const typography = css`
   body {
-    font-family: Arial, sans-serif;
+    font-family: 'Fredoka', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: ${({ theme: { fontSize } }) => fontSize.normal};
     font-weight: ${({ theme: { fontWeight } }) => fontWeight.normal};
     line-height: ${({ theme: { lineHeight } }) => lineHeight.normal};
     letter-spacing: 0.05rem;
+    color: ${({ theme: { colors } }) => colors.textPrimary};
+  }
+
+  a {
     color: ${({ theme: { colors } }) => colors.textPrimary};
   }
 `;
