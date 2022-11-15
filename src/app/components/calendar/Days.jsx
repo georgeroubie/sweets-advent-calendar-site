f7567b9734c09ab437baff2c1b655277e8e45feb
George Roubie <george.roubie@gmail.com>
2022-11-16 00:29:35 +0200

    Update Days.jsx

--- src/app/components/calendar/Days.jsx
+++ src/app/components/calendar/Days.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const DaysList = styled.div`
@@ -27,6 +27,13 @@ const Days = ({ setActiveRecipe }) => {
   const [height, setHeight] = useState(0);
   const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
+  const [currentDay, currentMonth] = useMemo(() => {
+    const date = new Date();
+    return [date.getDate(), date.getMonth() + 1];
+  }, []);
+
+  console.log(currentDay);
+
   useEffect(() => {
     function changeHeightValue() {
       // Make all the days in the calendar squares
@@ -44,10 +51,25 @@ const Days = ({ setActiveRecipe }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  function handleDayClick(d) {
+    if (currentMonth !== 11) {
+      alert('Wait until December, be a good kid because santa is watching!');
+      return;
+    }
+
+    console.log(currentDay, d);
+    if (d > currentDay) {
+      alert(`Wait until ${d} December to get your recipe, don't be naughty!`);
+      return;
+    }
+
+    setActiveRecipe(d);
+  }
+
   return (
     <DaysList>
       {days.map((d) => (
-        <Day key={d} ref={dayRef} style={{ height }} onClick={() => setActiveRecipe(d)}>
+        <Day key={d} ref={dayRef} style={{ height }} onClick={() => handleDayClick(d)}>
           {d}
         </Day>
       ))}
