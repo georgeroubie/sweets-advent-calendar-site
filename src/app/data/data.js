5524009081ced5ed58401fe291e660cd2783eab7
George Roubie <george.roubie@gmail.com>
2022-11-25 15:33:04 +0200

    Create data.js

---
+++ src/app/data/data.js
@@ -0,0 +1,467 @@
+const recipes = [
+  // DEC 1
+  {
+    title: 'Caramel Macchiato Cheesecake',
+    ingredients: [
+      {
+        unit: '2 cups',
+        description: 'graham cracker crumbs',
+      },
+      {
+        unit: '1/2 cup',
+        description: 'butter, melted',
+      },
+      {
+        unit: '2 tablespoons',
+        description: 'white sugar',
+      },
+      {
+        unit: '3 (8 ounce) packages',
+        description: 'cream cheese, softened',
+      },
+      {
+        unit: '1 cup',
+        description: 'white sugar',
+      },
+      {
+        unit: '3',
+        description: 'eggs',
+      },
+      {
+        unit: '1 (8 ounce) container',
+        description: 'sour cream',
+      },
+      {
+        unit: '1/4 cup',
+        description: 'brewed espresso or strong coffee',
+      },
+      {
+        unit: '2 teaspoons',
+        description: 'vanilla extract',
+      },
+      {
+        description: 'pressurized whipped cream',
+      },
+      {
+        description: 'caramel ice cream topping',
+      },
+    ],
+    directions: [
+      {
+        subtitle: 'Step 1',
+        description:
+          'Preheat oven to 350 degrees F (175 degrees C). Lightly coat a 9-inch springform pan with nonstick cooking spray.',
+      },
+      {
+        subtitle: 'Step 2',
+        description:
+          'Mix together the graham cracker crumbs, melted butter, and 2 tablespoons of sugar until well combined. Press into the bottom of the prepared springform pan, and 1 inch up the sides. Bake in preheated oven for 8 minutes, then remove to cool on a wire rack.',
+      },
+      {
+        subtitle: 'Step 3',
+        description: 'Reduce oven temperature to 325 degrees F (165 degrees C).',
+      },
+      {
+        subtitle: 'Step 4',
+        description:
+          'Beat the softened cream cheese in a large bowl with an electric mixer until fluffy. Gradually add 1 cup of sugar, beating until blended. Add eggs one at a time, beating well after each addition. Stir in sour cream, espresso and vanilla. Pour batter into the baked and cooled crust.',
+      },
+      {
+        subtitle: 'Step 5',
+        description:
+          'Bake cheesecake in the preheated oven for 1 hour and 5 minutes; then turn the oven off, partially open the door and allow the cheesecake to rest for 15 minutes more. Remove from the oven, and run a knife around the edges. Cool cheesecake on a wire rack to room temperature, then cover the springform pan with plastic wrap, and chill in the refrigerator for 8 hours.',
+      },
+      {
+        subtitle: 'Step 6',
+        description:
+          'To serve, cut the cheesecake into wedges and garnish each slice with whipped cream and caramel sauce.',
+      },
+    ],
+  },
+  // DEC 2
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 3
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 4
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 5
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 6
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 7
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 8
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 9
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 10
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 11
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 12
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 13
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 14
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 15
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 16
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 17
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 18
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 19
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 20
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 21
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 22
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 23
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 24
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+  // DEC 25
+  {
+    title: '',
+    ingredients: [
+      {
+        unit: '',
+        description: '',
+      },
+    ],
+    directions: [
+      {
+        subtitle: '',
+        description: '',
+      },
+    ],
+  },
+];
+
+export { recipes };
