e6bfc0020c9dea71a4ec30db2326ad988f5f2456
George Roubie <george.roubie@gmail.com>
2022-11-25 15:31:58 +0200

    Update Recipes.jsx

--- src/app/components/calendar/Recipes.jsx
+++ src/app/components/calendar/Recipes.jsx
@@ -1,67 +1,27 @@
 import PropTypes from 'prop-types';
 import { useMemo } from 'react';
-import DecemberRecipe1 from '../recipes/DecemberRecipe1';
-import DecemberRecipe10 from '../recipes/DecemberRecipe10';
-import DecemberRecipe11 from '../recipes/DecemberRecipe11';
-import DecemberRecipe12 from '../recipes/DecemberRecipe12';
-import DecemberRecipe13 from '../recipes/DecemberRecipe13';
-import DecemberRecipe14 from '../recipes/DecemberRecipe14';
-import DecemberRecipe15 from '../recipes/DecemberRecipe15';
-import DecemberRecipe16 from '../recipes/DecemberRecipe16';
-import DecemberRecipe17 from '../recipes/DecemberRecipe17';
-import DecemberRecipe18 from '../recipes/DecemberRecipe18';
-import DecemberRecipe19 from '../recipes/DecemberRecipe19';
-import DecemberRecipe2 from '../recipes/DecemberRecipe2';
-import DecemberRecipe20 from '../recipes/DecemberRecipe20';
-import DecemberRecipe21 from '../recipes/DecemberRecipe21';
-import DecemberRecipe22 from '../recipes/DecemberRecipe22';
-import DecemberRecipe23 from '../recipes/DecemberRecipe23';
-import DecemberRecipe24 from '../recipes/DecemberRecipe24';
-import DecemberRecipe25 from '../recipes/DecemberRecipe25';
-import DecemberRecipe3 from '../recipes/DecemberRecipe3';
-import DecemberRecipe4 from '../recipes/DecemberRecipe4';
-import DecemberRecipe5 from '../recipes/DecemberRecipe5';
-import DecemberRecipe6 from '../recipes/DecemberRecipe6';
-import DecemberRecipe7 from '../recipes/DecemberRecipe7';
-import DecemberRecipe8 from '../recipes/DecemberRecipe8';
-import DecemberRecipe9 from '../recipes/DecemberRecipe9';
+import { recipes } from '../../data/data';
+import Container from '../layout/Container';
+import Title from '../typography/Title';
+import CheckList from '../ui/CheckList';
+import StepsList from '../ui/StepsList';
 
 const Recipes = ({ activeRecipe, setActiveRecipe }) => {
-  const RecipesComponents = useMemo(() => {
-    return [
-      DecemberRecipe1,
-      DecemberRecipe2,
-      DecemberRecipe3,
-      DecemberRecipe4,
-      DecemberRecipe5,
-      DecemberRecipe6,
-      DecemberRecipe7,
-      DecemberRecipe8,
-      DecemberRecipe9,
-      DecemberRecipe10,
-      DecemberRecipe11,
-      DecemberRecipe12,
-      DecemberRecipe13,
-      DecemberRecipe14,
-      DecemberRecipe15,
-      DecemberRecipe16,
-      DecemberRecipe17,
-      DecemberRecipe18,
-      DecemberRecipe19,
-      DecemberRecipe20,
-      DecemberRecipe21,
-      DecemberRecipe22,
-      DecemberRecipe23,
-      DecemberRecipe24,
-      DecemberRecipe25,
-    ];
-  }, []);
+  const recipe = useMemo(() => {
+    return recipes[activeRecipe - 1];
+  }, [activeRecipe]);
 
-  const Recipe = useMemo(() => {
-    return RecipesComponents[activeRecipe - 1];
-  }, [activeRecipe, RecipesComponents]);
+  function resetActiveRecipe() {
+    setActiveRecipe(undefined);
+  }
 
-  return <Recipe setActiveRecipe={setActiveRecipe} />;
+  return (
+    <Container>
+      <Title onBack={resetActiveRecipe}>{recipe.title}</Title>
+      <CheckList id={`ingredients_dec_${activeRecipe}`} title="Ingredients" list={recipe.ingredients} />
+      <StepsList id={`directions_dec_${activeRecipe}`} title="Directions" list={recipe.directions} />
+    </Container>
+  );
 };
 
 Recipes.propTypes = {
