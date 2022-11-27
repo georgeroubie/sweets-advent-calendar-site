78e0e4c56ae4d8af797c4b045cf249add8012e94
George Roubie <george.roubie@gmail.com>
2022-11-27 15:06:33 +0200

    Update Recipes.jsx

--- src/app/components/calendar/Recipes.jsx
+++ src/app/components/calendar/Recipes.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 import { useMemo } from 'react';
+import styled from 'styled-components';
 import { recipes } from '../../data/data';
 import Container from '../layout/Container';
+import Description from '../typography/Description';
 import Title from '../typography/Title';
 import CheckList from '../ui/CheckList';
 import StepsList from '../ui/StepsList';
+
+const Link = styled.a``;
 
 const Recipes = ({ activeRecipe, setActiveRecipe }) => {
   const recipe = useMemo(() => {
@@ -20,6 +24,13 @@ const Recipes = ({ activeRecipe, setActiveRecipe }) => {
       <Title onBack={resetActiveRecipe}>{recipe.title}</Title>
       <CheckList id={`ingredients_dec_${activeRecipe}`} title="Ingredients" list={recipe.ingredients} />
       <StepsList id={`directions_dec_${activeRecipe}`} title="Directions" list={recipe.directions} />
+      <Description>
+        You can see the original recipe&nbsp;
+        <Link href={recipe.url} target="_blank" rel="noreferrer">
+          here
+        </Link>
+        .
+      </Description>
     </Container>
   );
 };
