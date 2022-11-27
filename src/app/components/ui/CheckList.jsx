8ae8189ce4d88d818ebdc9fbcc4b9f8b16849567
George Roubie <george.roubie@gmail.com>
2022-11-27 15:06:40 +0200

    Update CheckList.jsx

--- src/app/components/ui/CheckList.jsx
+++ src/app/components/ui/CheckList.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
+import _SmallSubtitle from '../typography/SmallSubtitle';
 import Subtitle from '../typography/Subtitle';
 import Checkbox from './Checkbox';
 
 const List = styled.div`
   margin-bottom: ${({ theme: { spacing } }) => spacing.xlarge};
+`;
+
+const SmallSubtitle = styled(_SmallSubtitle)`
+  margin-top: ${({ theme: { spacing } }) => spacing.normal};
 `;
 
 const Unit = styled.strong``;
@@ -13,17 +19,21 @@ const Description = styled.span``;
 
 const CheckList = ({ id, title, list }) => {
   return (
-    <>
+    <List>
       {title && <Subtitle>{title}</Subtitle>}
-      <List>
-        {list.map(({ unit, description }, index) => (
-          <Checkbox key={index} id={`${id}_list_item_${index}`}>
-            {unit && <Unit>{unit}&nbsp;</Unit>}
-            <Description>{description}</Description>
-          </Checkbox>
-        ))}
-      </List>
-    </>
+      {list.map((item, index) => (
+        <React.Fragment key={index}>
+          {item.title ? (
+            <SmallSubtitle>{item.title}</SmallSubtitle>
+          ) : (
+            <Checkbox id={`${id}_list_item_${index}`}>
+              {item.unit && <Unit>{item.unit}&nbsp;</Unit>}
+              <Description>{item.description}</Description>
+            </Checkbox>
+          )}
+        </React.Fragment>
+      ))}
+    </List>
   );
 };
 
@@ -32,8 +42,9 @@ CheckList.propTypes = {
   title: PropTypes.string,
   list: PropTypes.arrayOf(
     PropTypes.shape({
+      title: PropTypes.string,
       unit: PropTypes.string,
-      description: PropTypes.string.isRequired,
+      description: PropTypes.string,
     }),
   ).isRequired,
 };
