85b5734d1ff898f6bf0c236492e7ac5c00776a55
George Roubie <george.roubie@gmail.com>
2022-12-01 18:36:51 +0200

    Update Checkbox.jsx

--- src/app/components/ui/Checkbox.jsx
+++ src/app/components/ui/Checkbox.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: ${({ theme: { spacing } }) => spacing.xsmall};
 `;
 
 const CheckboxInput = styled.input`
